b0a76ea2d28c113bb193abe72b7c9e9cbfa24ed1
Glitch (maquetado-grid-2018martes15-02) <none>
2018-07-10 20:31:20 +0000

    🍔💎 Updated with Glitch

--- cliente.js
+++ cliente.js
@@ -12,7 +12,8 @@ class Distribuidora {
       += document.querySelector("#golosina_titulo").value
       + document.querySelector("#golosina_descripcion").value;
 
-    
+    document.querySelector("#golosina_titulo").value = "";
+    document.querySelector("#golosina_descripcion").value = "";
 //    alert(12345678);
     
     console.log('[OK] Programando en Javascript en Buenos Aires');
